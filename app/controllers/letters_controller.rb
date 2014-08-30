2c38301b14848f9a1c234feece16bca4816f856c
aymeric.bouzy@gmail.com <aymeric.bouzy@applidget.com>
2014-08-30 11:14:53 +0200

    removed unallowed operations

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -12,63 +12,9 @@ class LettersController < ApplicationController
   def show
   end
 
-  # GET /letters/new
-  def new
-    @letter = Letter.new
-  end
-
-  # GET /letters/1/edit
-  def edit
-  end
-
-  # POST /letters
-  # POST /letters.json
-  def create
-    @letter = Letter.new(letter_params)
-
-    respond_to do |format|
-      if @letter.save
-        format.html { redirect_to @letter, notice: 'Letter was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @letter }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @letter.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /letters/1
-  # PATCH/PUT /letters/1.json
-  def update
-    respond_to do |format|
-      if @letter.update(letter_params)
-        format.html { redirect_to @letter, notice: 'Letter was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @letter.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /letters/1
-  # DELETE /letters/1.json
-  def destroy
-    @letter.destroy
-    respond_to do |format|
-      format.html { redirect_to letters_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_letter
       @letter = Letter.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def letter_params
-      params[:letter]
     end
 end

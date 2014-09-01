0970238884f3482df4b47d4d2aef115fb39f6773
aymeric.bouzy@gmail.com <aymeric.bouzy@applidget.com>
2014-09-01 22:44:28 +0200

    wip - adding html template

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "active_resource/railtie" # Comment this line for Rails 4.0+
 require "rails/test_unit/railtie"
-# require "sprockets/railtie" # Uncomment this line for Rails 3.1+
+require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
